be0184d9026c142aeeda6b4c267b0aa0902d2973
yqc <yqc@192.168.0.102>
2016-01-15 12:08:40 +0800

    modify jni interface and README

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # log_jni
 For android jni: write log in sdcard
+
+in logJNI.cpp
+#define FILE_NUM_MAX 50
+#define FILE_SIZE_MAX 1024*200 // 200KB
+
+FILE_NUM_MAX： the max number of file, if outnumber, delete the oldest one
+FILE_SIZE_MAX: the max size before write
+
+functions:
+stringFromJNI  // for test
+writeFile(String filePath, String content);
+
--- src/com/yzayqc/android/LogJNILib.java
+++ src/com/yzayqc/android/LogJNILib.java
@@ -16,13 +16,13 @@ public class LogJNILib {
 
 	public native static String stringFromJNI();
 
-	public native static void nativeInit();
+//	public native static void nativeInit();
 
-	public native static void nativeFree();
+//	public native static void nativeFree();
 	
 	public native static void writeFile(String filePath, String content);
 
-	public static void clearCash() {
-		nativeFree();
-	}
+//	public static void clearCash() {
+//		nativeFree();
+//	}
 }
